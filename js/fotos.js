bffa4153ee008423fec1f8d317e650e9cdc92f76
LeahJKH <leahjb06.kodehode@gmail.com>
2025-06-18 12:35:17 +0200

    changed some styling

---
+++ js/fotos.js
@@ -0,0 +1,192 @@
+const images = [
+  {
+    src: "../images/moreimages/baaterlang.webp",
+    alt: "her ser vi båter som blir brukt til blantannet fisking eller å sjekke på hav ørnene",
+  },
+  {
+    src: "../images/moreimages/brua.webp",
+    alt: "her er et langsiktig bilde av telavågs brua",
+  },
+  {
+    src: "../images/moreimages/bruaInn.webp",
+    alt: "her er innsjøen med brua det er også en fin bade plass",
+  },
+  {
+    src: "../images/moreimages/bruaUnder.webp",
+    alt: "her er bilder av naustene innover brua",
+  },
+  {
+    src: "../images/moreimages/bruaUnder2.webp",
+    alt: "her kan man se båter kjøre ofte over dagen",
+  },
+  {
+    src: "../images/moreimages/bruaUt.webp",
+    alt: "her er utsikten ut fra brua mot sjøen",
+  },
+  {
+    src: "../images/moreimages/bunker.webp",
+    alt: "her er bilde av en gammel bunker du kan finne mange av di utover telavåg",
+  },
+  {
+    src: "../images/moreimages/båterlang2.webp",
+    alt: "bryggen med kaien her er der gjester ofte kommer",
+  },
+  {
+    src: "../images/moreimages/fotballOverveiw.webp",
+    alt: "her er et bilde over stadioen våres",
+  },
+  {
+    src: "../images/moreimages/havnlang.webp",
+    alt: "dette er kaien her samlest ofte folk om sommeren",
+  },
+  {
+    src: "../images/moreimages/havnut.webp",
+    alt: "fra kaien kan du se museumet",
+  },
+  {
+    src: "../images/moreimages/gamleskole.webp",
+    alt: "dette er telavåg skule den ble lagt ned for 3 år siden",
+  },
+  {
+    src: "../images/moreimages/himmel1.webp",
+    alt: "Du kan ofte godt se den nydlige himmelen",
+  },
+  {
+    src: "../images/moreimages/himmel2.webp",
+    alt: "",
+  },
+  {
+    src: "../images/moreimages/himmel3.webp",
+    alt: "",
+  },
+  {
+    src: "../images/moreimages/idrettsplassH.webp",
+    alt: "her er idretsplassen med bygningen her er det fritt for alle å komme",
+  },
+  {
+    src: "../images/moreimages/idrettsplassV.webp",
+    alt: "grus banen blir brukt til mye som fotball, hester og andre aktiviteter",
+  },
+  {
+    src: "../images/moreimages/kanonutstilling.webp",
+    alt: "gammle kanonutstillingen er per idag blitt en bryllups plass",
+  },
+  {
+    src: "../images/moreimages/kanonutstilling2.webp",
+    alt: "det er en benk der så du kan se ut over havet",
+  },
+  {
+    src: "../images/moreimages/kanonutstillingfull.webp",
+    alt: "her kan du se folk om sommeren grille og av og til er det et bryllup her",
+  },
+  {
+    src: "../images/moreimages/kanonutstillingutsikt.webp",
+    alt: "utsikten fra kanonutstillingen",
+  },
+  {
+    src: "../images/moreimages/kanonutstillingutsikt2.webp",
+    alt: "du får god uttsikt over himmelen fra kanonutstillingen",
+  },
+  {
+    src: "../images/moreimages/kanonutstillingVeg.webp",
+    alt: "her har vi veien til gapahuken",
+  },
+  {
+    src: "../images/moreimages/kanonutstillingVeg2.webp",
+    alt: "her er veien til kanonutstillingen. di er begge ganske natur rik",
+  },
+  {
+    src: "../images/moreimages/museumLong.webp",
+    alt: "her har vi museumet fra lang distanse",
+  },
+  {
+    src: "../images/moreimages/museumLongR.webp",
+    alt: "museumet har en lang vei opp med gamle vann miner på veien",
+  },
+  {
+    src: "../images/moreimages/naust1.webp",
+    alt: "her har vi endo et bilde fra brua med bedre utsikt av fjellet",
+  },
+  {
+    src: "../images/moreimages/naustbaater.webp",
+    alt: "her har vi bilder av noen av båtene på venstre side av kaien",
+  },
+  {
+    src: "../images/moreimages/naustbaater2.webp",
+    alt: "her er båtene på høgre side",
+  },
+  {
+    src: "../images/moreimages/nipa1.webp",
+    alt: "her har vi nipafjellet fra avstand",
+  },
+  {
+    src: "../images/moreimages/nipafjell.webp",
+    alt: "her har vi venstre siden av nipa fjellet",
+  },
+  {
+    src: "../images/moreimages/nipafjellb.webp",
+    alt: "her har vi bunnen av nipa fjellet",
+  },
+  {
+    src: "../images/moreimages/nipafjellm.webp",
+    alt: "her er høgre side av nipadjellet",
+  },
+  {
+    src: "../images/moreimages/skilt.webp",
+    alt: "vi har mange tur stier makert med disse skiltene",
+  },
+  {
+    src: "../images/moreimages/utsiktHav.webp",
+    alt: "her er et zoomet bilde fra gapahuken",
+  },
+  {
+    src: "../images/moreimages/utsiktHavLang.webp",
+    alt: "her er den fulle bilde fra gapahuken",
+  },
+  {
+    src: "../images/moreimages/vaatmark.webp",
+    alt: "naturen våres er nesten helt urørt og du kan finne ganske mange dammer eller myrer",
+  },
+  {
+    src: "../images/moreimages/vaatmarkSkog.webp",
+    alt: "fra den dammen kan du se utover til en skog",
+  },
+  {
+    src: "../images/moreimages/vannetG.webp",
+    alt: "dette vannet har vert der så lenge jeg har bod der og den har ender",
+  },
+  {
+    src: "../images/moreimages/vidfjell.webp",
+    alt: "her er et fjell fra avstand",
+  },
+  {
+    src: "../images/moreimages/vidfjellLang.webp",
+    alt: "her er endo et fjell fra avstand",
+  },
+  {
+    src: "../images/moreimages/Minnesmerke.webp",
+    alt: "her står navnene på di som døde under telavåg tragedien",
+  },
+  {
+    src: "../images/moreimages/Ruiner.webp",
+    alt: "dette var bygget av en telavåger til minne for telavåg tragedien",
+  },
+  {
+    src: "../images/moreimages/gamleskolebane.webp",
+    alt: "her kan man spille fotball eller så kan man ha hundene sine her for å leke",
+  },
+  {
+    src: "../images/moreimages/ungdomshuset.webp",
+    alt: "dette er der vi samles for forskjellige ting som konsert med øs5 eller juleverksted også videre",
+  },
+];
+
+const Container = document.querySelector("#imagesCont");
+
+images.forEach((e) => {
+  const image = document.createElement("img");
+  image.src = e.src;
+  image.alt = e.alt;
+  image.classList.add("imagePos");
+  Container.appendChild(image);
+});
